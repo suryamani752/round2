d27c6beada6bd0c654ba4cbddc7d119d7310f21c
Suryamani kumar <124435021+suryamani752@users.noreply.github.com>
2023-09-19 17:29:59 +0530

    first commit

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/bfhl', (req, res) => {
-
+app.post("/bfhl", (req, res) => {
   const requestData = req.body.data;
-const numbers = [];
+  const numbers = [];
   const alphabets = [];
 
-  requestData.forEach(item => {
+  requestData.forEach((item) => {
     if (/[a-zA-Z]/.test(item)) {
-        alphabets.push(item);
+      alphabets.push(item);
     } else if (/[0-9]/.test(item)) {
-        numbers.push(item);
+      numbers.push(item);
+    } else {
+      res.status(400).json({
+        is_sucess: false,
+        message: "Invalid user input",
+      });
     }
-    else{
-        res.status(400).json({
-            "is_sucess": false,
-            "message": "Invalid user input"
-        })
-    }
-    });
+  });
 
-  const user_id = 'suryamani_kumar07052002';
-  const email = 'suryamani.kumar2020@vitbhopal.ac.in';
-  const roll_number = '20BCE10080';
+  const user_id = "suryamani_kumar07052002";
+  const email = "suryamani.kumar2020@vitbhopal.ac.in";
+  const roll_number = "20BCE10080";
 
-  const highest_alphabet = alphabets.reduce((max, current) => max > current ? max : current, '');
+  const highest_alphabet = alphabets.reduce(
+    (max, current) => (max > current ? max : current),
+    ""
+  );
   const response = {
-    "is_success": true,
+    is_success: true,
     user_id,
     email,
     roll_number,
@@ -42,10 +43,8 @@ const numbers = [];
   res.json(response);
 });
 
-app.get('/bfhl', (req, res) => {
-  
-  const operation_code = 1; 
-
+app.get("/bfhl", (req, res) => {
+  const operation_code = 1;
 
   res.json({ operation_code });
 });
